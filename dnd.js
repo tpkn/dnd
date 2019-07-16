c4c7c955e2c953d27d39813b6d86058e0829e26a
tpkn <tpkn@users.noreply.github.com>
2019-07-16 14:59:27 +0300

    Add files via upload

--- dnd.js
+++ dnd.js
@@ -3,53 +3,53 @@
  */
 
 function DnD(target, options){
-	if(typeof target !== 'object' || target == null){
-		throw new TypeError('No target element');
-	}
+   if(typeof target !== 'object' || target == null){
+      throw new TypeError('No target element');
+   }
 
-	var drop = typeof options.drop !== 'function' ? function(e){} : options.drop;
-	var over = typeof options.over !== 'function' ? function(e){} : options.over;
-	var out =  typeof options.out  !== 'function' ? function(e){} : options.out;
+   var drop = typeof options.drop !== 'function' ? function(e){} : options.drop;
+   var over = typeof options.over !== 'function' ? function(e){} : options.over;
+   var out =  typeof options.out  !== 'function' ? function(e){} : options.out;
 
-	target.ondrop = function(e){
-		e.preventDefault();
+   target.ondrop = function(e){
+      e.preventDefault();
 
-		var files = e.dataTransfer.files;
-		var text = e.dataTransfer.getData('text');
-		
-		drop(e, files, text);
-		out(e);
-	};
+      var files = e.dataTransfer.files;
+      var text = e.dataTransfer.getData('text');
+      
+      drop(e, files, text);
+      out(e);
+   };
 
-	target.ondragover = function(e){
-		e.preventDefault();
-		over(e);
-	};
+   target.ondragover = function(e){
+      e.preventDefault();
+      over(e);
+   };
 
-	target.ondragenter = function(e){
-		e.preventDefault();
-		over(e);
-	};
+   target.ondragenter = function(e){
+      e.preventDefault();
+      over(e);
+   };
 
-	target.ondragleave = function(e){
-		e.preventDefault();
-		out(e);
-	};
+   target.ondragleave = function(e){
+      e.preventDefault();
+      out(e);
+   };
 
-	target.ondragend = function(e){
-		e.preventDefault();
-		out(e);
-	};
+   target.ondragend = function(e){
+      e.preventDefault();
+      out(e);
+   };
 }
 
 if(typeof module !== 'undefined' && typeof module.exports !== 'undefined'){
-	module.exports = DnD;
+   module.exports = DnD;
 }else{
-	if(typeof define === 'function' && define.amd){
-		define([], function(){
-			return DnD;
-		});
-	}else{
-		window.DnD = DnD;
-	}
+   if(typeof define === 'function' && define.amd){
+      define([], function(){
+         return DnD;
+      });
+   }else{
+      window.DnD = DnD;
+   }
 }
